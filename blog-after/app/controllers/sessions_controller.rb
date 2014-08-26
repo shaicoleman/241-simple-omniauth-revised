3dd9605ce5efe51d5c187349c4965c7880d8f68b
Shai Coleman <git@shaicoleman.com>
2014-08-26 15:31:16 +0100

    Allow to deauthorise LinkedIn

--- blog-after/app/controllers/sessions_controller.rb
+++ blog-after/app/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 class SessionsController < ApplicationController
-  def create
+  def linkedin_create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     flash[:success] = "Logged in!"
-    return render :close_window if params[:oauth_token].present?
+    return render :close_window, layout: false if params[:oauth_token].present?
+    return redirect_to :root
+  end
+
+  def linkedin_destroy
+    if current_user && current_user.linkedin_id.present?
+      #current_user.linkedin_id = nil
+      #current_user.linkedin_data = nil
+      current_user.destroy
+      session[:user_id] = nil
+    end
+    flash[:success] = 'LinkedIn connection removed, you may wish to <a href="https://www.linkedin.com/secure/settings?userAgree" target="_blank">deauthorise the app</a>'.html_safe
     return redirect_to :root
   end
   
--- blog-after/config/routes.rb
+++ blog-after/config/routes.rb
@@ -1,7 +1,8 @@
 Blog::Application.routes.draw do
   resources :articles
   root to: 'articles#index'
-  match 'auth/linkedin/callback', to: 'sessions#create'
+  match 'auth/linkedin/callback', to: 'sessions#linkedin_create'
+  match 'auth/linkedin/destroy', to: 'sessions#linkedin_destroy'
   match 'auth/failure', to: redirect('/')
   match 'logout', to: 'sessions#destroy', as: 'logout'
 end
